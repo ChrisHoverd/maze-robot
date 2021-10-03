0b2088ca7742fa425cdb85c7bc12d3b3fd21fd4e
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-10-02 21:26:11 -0400

    took out excess code not needed for the test

--- counter_to_mm_test/counter_to_mm_test.ino
+++ counter_to_mm_test/counter_to_mm_test.ino
@@ -1,25 +1,6 @@
-/*
-    Maze Robot 
+//use this code to conduct required tests to receive good readings for mm/pulses
 
-    The robot solves a maze.
-
-    Components:
-    DC-DC Power Module 25W (DFR0205)                                               x 1
-    Pololu 5V, 2.5A Step-Down Voltage Regulator                                    x 1
-    0.8Amp 5V-26V DC Motor Driver Shield for Arduino (2 Channels) (SHIELD-2AMOTOR) x 1
-    Micro DC Motor with Encoder (FIT0458)                                          x 2
-    GOLDBAT 1500mAh 11.1V 100C 3S Lipo Battery                                     x 1
-
-    Pin Connections:
-
-
-    Created 9/26/2021
-    Christopher Hoverd
-
-    https://github.com/ChrisHoverd/maze-robot
-*/
-
-//include motor driver library
+//include libraries
 #include "CytronMotorDriver.h" 
 #include "Encoder.h"
 #include "TimerThree.h"
@@ -31,9 +12,6 @@ const int m1_ch_B = 3;
 const int m2_ch_A = 18;
 const int m2_ch_B = 19;
 
-//declare mm/pulse constant
-const float pulses_to_mm = 0.108854;
-
 //instantiate the encoder objects
 Encoder m1_Enc(m1_ch_A, m1_ch_B);
 Encoder m2_Enc(m2_ch_A, m2_ch_B);
@@ -43,16 +21,9 @@ Encoder m2_Enc(m2_ch_A, m2_ch_B);
 volatile long m1_counter = 0;
 volatile long m2_counter = 0;
 
-int distance;
-
 //instantiate the motor objects
 CytronMD motor1(PWM_DIR, 5, 4); // Motor 1 EN = Pin 5, DIR = 4
 CytronMD motor2(PWM_DIR, 6, 7); // Motor 2 EN = Pin 6, Dir = 7
-
-
-//declare motor directions, ie. CW or CCW
-String m1_direction = "";
-String m2_direction = "";
 
 //declare motor PWM values, ie. -255 to 255
 int m1_PWM = 60;
@@ -76,19 +47,27 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+    delay(5000);
     motor1.setSpeed(m1_PWM);
     motor2.setSpeed(m2_PWM);
 
-    delay(2000);
+    delay(3000);
     motor1.setSpeed(0);
     motor2.setSpeed(0);
-    m1_counter = m1_Enc.read();
-    m2_counter = m2_Enc.read();
-    Serial.print("M1 Counter: ");
-    Serial.println(m1_counter);
-    Serial.print("M2 Counter: ");
-    Serial.println(m2_counter);
-    delay(10000);
+    
+    while(true)
+
+    {
+      m1_counter = m1_Enc.read();
+      m2_counter = m2_Enc.read();
+      Serial.print("M1 Counter: ");
+      Serial.print(m1_counter);
+      Serial.print("  |  ");
+      Serial.print("M2 Counter: ");
+      Serial.println(m2_counter);
+      delay(3000);
+    }
+
 
 }
 
