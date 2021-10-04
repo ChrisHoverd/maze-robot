16acd02697b3b0ddc69791753dc4d996db1b9011
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-10-04 00:41:18 -0400

    integrated Ir-tests into a function in main

--- main/main.ino
+++ main/main.ino
@@ -10,7 +10,7 @@
     Micro DC Motor with Encoder (FIT0458)                                          x 2
     GOLDBAT 1500mAh 11.1V 100C 3S Lipo Battery                                     x 1
 
-    Pin Connections:
+    Pin Connections: 
 
 
     Created 9/26/2021
@@ -31,7 +31,16 @@ const int m1_ch_B = 3;
 const int m2_ch_A = 18;
 const int m2_ch_B = 19;
 
+//declare ir values
+int left_ir_pin = A0;
+int left_ir_val = 0;
+float sensor_sample = 10;
+float left_ir_sum = 0;
+float left_ir_average_val = 0;
+float left_ir_distance = 0;
+
 //declare mm/pulse constant
+//converts encoder ticks to mm
 const float pulses_to_mm = 0.05283198477;
 
 //instantiate the encoder objects
@@ -42,6 +51,16 @@ Encoder m2_Enc(m2_ch_A, m2_ch_B);
 //declare encoder counter variables
 volatile long m1_counter = 0;
 volatile long m2_counter = 0;
+volatile double m1_delta = 0;
+volatile double m2_delta = 0;
+
+volatile double orientation = 0;
+volatile double delta_orientation = 0;
+volatile double delta_distance = 0;
+volatile double xPosition = 0;
+volatile double yPosition = 0;
+
+volatile 
 
 float distance;
 
@@ -50,15 +69,14 @@ CytronMD motor1(PWM_DIR, 5, 4); // Motor 1 EN = Pin 5, DIR = 4
 CytronMD motor2(PWM_DIR, 6, 7); // Motor 2 EN = Pin 6, Dir = 7
 
 
-//declare motor directions, ie. CW or CCW
-String m1_direction = "";
-String m2_direction = "";
 
 //declare motor PWM values, ie. -255 to 255
-int m1_PWM = 70;
-int m2_PWM = 70;
+int m1_PWM = 60;
+int m2_PWM = 60;
 
+//declare wheel odometry variables
 
+int wheelbase = 96;
 void setup() {
   // setup code runs once at the beginning of the program
 
@@ -68,6 +86,9 @@ void setup() {
   pinMode (m2_ch_A, INPUT_PULLUP);
   pinMode (m2_ch_B, INPUT_PULLUP);
   
+  //sets the reference voltage for the analog inputs, ie. 3.3V from the arduino for the IR sensors
+  analogReference(EXTERNAL);
+
   //sets the data rate in bits per second (baud)
   Serial.begin(9600);
 
@@ -75,27 +96,48 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    
     motor1.setSpeed(m1_PWM);
     motor2.setSpeed(m2_PWM);
 
-    while(distance<400)
+    while(distance<200)
+    {
+      m1_counter = m1_Enc.read();
+      distance = abs((m1_counter*pulses_to_mm));
+    }
+
+    motor1.setSpeed(0);
+    motor2.setSpeed(0);
+    delay(2000);
+    m1_PWM = 60;
+    m2_PWM = -60;
+    motor1.setSpeed(m1_PWM);
+    motor2.setSpeed(m2_PWM);
+    distance = 0;
+    m1_Enc.readAndReset();
+    while(distance<96)
     {
       m1_counter = m1_Enc.read();
       distance = abs((m1_counter*pulses_to_mm));
     }
     motor1.setSpeed(0);
     motor2.setSpeed(0);
-    Serial.println(distance);
-    delay(10000);
-    // m1_counter = m1_Enc.read();
-    // m2_counter = m2_Enc.read();
-    // Serial.print("M1 Counter: ");
-    // Serial.println(m1_counter);
-    // Serial.print("M2 Counter: ");
-    // Serial.println(m2_counter);
-
+  delay(100000);
 }
 
+void leftIRCalc()
+{
+    for(int i = 0; i<sensor_sample; ++i)
+  {
+    left_ir_sum+= analogRead(left_ir_pin);
+  }
 
+  left_ir_average_val = left_ir_sum/sensor_sample;
+  Serial.print("Sensor Value: ");
+  Serial.println(left_ir_average_val);
+  left_ir_distance = (-0.0903*left_ir_average_val + 65.306)*10; //analog to mm function
+  Serial.print("Distance in cm: ");
+  Serial.println(left_ir_distance); 
+  left_ir_sum = 0;
+}
 
