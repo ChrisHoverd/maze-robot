ab5f7b5f7e1f5dbcdf76984149dfb95fefdb5c4c
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-10-17 15:59:57 -0400

    updated documentation

--- hardcoded-phaseA/hardcoded-phaseA.ino
+++ hardcoded-phaseA/hardcoded-phaseA.ino
@@ -1,6 +1,8 @@
-//hardcoded phase A
+/*
+  The main purpose of this code is to solve a maze via wheel odometry
+*/
 
-//include motor driver library
+//include motor driver and encoder libraries
 #include "CytronMotorDriver.h" 
 #include "Encoder.h"
 
@@ -15,6 +17,7 @@ const int right_motor_ch_B = 19;
 //converts encoder ticks to mm
 const float thick_wheels_pulses_to_mm = 0.054048211; //thick wheels
 const float skinny_wheels_pulses_to_mm = 0.06222775; // skinny wheels
+
 //instantiate the encoder objects
 Encoder left_Enc(left_motor_ch_B, left_motor_ch_A); // swapped channels A & B so that both motors have positive readings when moving forward
 Encoder right_Enc(right_motor_ch_A, right_motor_ch_B);
@@ -114,65 +117,112 @@ forward(205);
 }
 
 // **************   pick up miner  **********
+
+
+//this function will drive the robot forward a distance that is sent by the function call in the main loop
 void forward(double mm)
 { 
 
+  // set the speed variables of both motors
   left_motor_speed = 80;
   right_motor_speed = 80;
+  //reset the distance variable to 0
   float distance = 0;
+
+  //send the speed variables to the motors
   leftMotor.setSpeed(left_motor_speed);
   rightMotor.setSpeed(right_motor_speed);
+
+  //reset the motor encoder count
   left_Enc.readAndReset();
 
+
+  //while loop that runs until the set distance is reached
+  //the robot will move until it has reached a certain distance travelled
   while(distance<mm)
     {
+      //reads the encoder count
       left_motor_counter = left_Enc.read();
+
+      //updates the distance variable through a encoder count to mm conversion
       distance = abs((left_motor_counter*skinny_wheels_pulses_to_mm));
     }
 
+    //stop both motors once distance has been reached
     leftMotor.setSpeed(0);
     rightMotor.setSpeed(0);
+
+    //delay for 2 seconds
     delay(2000);
 }
 
+//this function will turn the robot 90 degrees to the left when called
 void leftTurn()
 { 
-
+    // set the speed variables of both motors
   left_motor_speed = -60;
   right_motor_speed = 60;
+    //reset the distance variable to 0
   float distance = 0;
+
+  //send the speed variables to the motors
   leftMotor.setSpeed(left_motor_speed);
   rightMotor.setSpeed(right_motor_speed);
+
+  //reset the motor encoder count
   left_Enc.readAndReset();
 
+  //while loop that runs until the set distance is reached
+  //the robot will move until it has reached a certain distance travelled
+  //98 mm was the arc length determined distance that is required to make a 90 degree turn left/right
   while(distance<98)
     {
+      //reads the encoder count
       left_motor_counter = left_Enc.read();
+      //updates the distance variable through a encoder count to mm conversion
       distance = abs((left_motor_counter*skinny_wheels_pulses_to_mm));
     }
 
+    // set the speed variables of both motors
     leftMotor.setSpeed(0);
     rightMotor.setSpeed(0);
+    //delay for 2 seconds
     delay(2000);
 }
 
+//this function will turn the robot 90 degrees to the right when called
 void rightTurn()
 { 
 
+  // set the speed variables of both motors
   left_motor_speed = 60;
   right_motor_speed = -60;
+
+  //reset the distance variable to 0
   float distance = 0;
+
+  //send the speed variables to the motors
   leftMotor.setSpeed(left_motor_speed);
   rightMotor.setSpeed(right_motor_speed);
+
+  //reset the motor encoder count
   left_Enc.readAndReset();
 
+  //while loop that runs until the set distance is reached
+  //the robot will move until it has reached a certain distance travelled
+  //98 mm was the arc length determined distance that is required to make a 90 degree turn left/right
   while(distance<98)
     {
+      //reads the encoder count
       left_motor_counter = left_Enc.read();
+      //updates the distance variable through a encoder count to mm conversion
       distance = abs((left_motor_counter*skinny_wheels_pulses_to_mm));
     }
 
+    // set the speed variables of both motors
     leftMotor.setSpeed(0);
     rightMotor.setSpeed(0);
+
+    //delay for 2 seconds
     delay(2000);
 }
