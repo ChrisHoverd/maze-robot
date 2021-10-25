e385072ddbba3f5cd2661c210449c683b85c07bb
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-10-25 17:05:22 -0400

    added GP2Y0A51SK0F analog to cm function

--- ir-tests/ir-tests.ino
+++ ir-tests/ir-tests.ino
@@ -43,20 +43,20 @@ void loop() {
 //  //Section 1
 //  //use this section to obtain data required for graphing distance and analog reading values
 
-  for(int i = 0; i<sensor_sample; ++i)
-  {
-  //sums an average of analog sensor readings
-    sensor_sum+= analogRead(ir_pin);
-  }
-  //takes the summed value and divides it by the number of samples
-  //prints the averaged sensor reading to the serial monitor
-  Serial.println(sensor_sum/sensor_sample); 
+  // for(int i = 0; i<sensor_sample; ++i)
+  // {
+  // //sums an average of analog sensor readings
+  //   sensor_sum+= analogRead(ir_pin);
+  // }
+  // //takes the summed value and divides it by the number of samples
+  // //prints the averaged sensor reading to the serial monitor
+  // Serial.println(sensor_sum/sensor_sample); 
 
-  //set the sum to 0
-  sensor_sum = 0;
+  // //set the sum to 0
+  // sensor_sum = 0;
 
-  //delay 2 seconds
-  delay(1000);
+  // //delay 2 seconds
+  // delay(1000);
 
 
 //***************************************************************************************************
@@ -64,31 +64,30 @@ void loop() {
   // Section 2
   // use below to test IR distance readings using determined analog reading to distance function
 
-  // for(int i = 0; i<sensor_sample; ++i)
-  // {
-  //   //sums an average of analog sensor readings
-  //   sensor_sum+= analogRead(ir_pin);
-  // }
-  // //takes the summed value and divides it by the number of samples
-  // average_sensor_val = sensor_sum/sensor_sample;
+  for(int i = 0; i<sensor_sample; ++i)
+  {
+    //sums an average of analog sensor readings
+    sensor_sum+= analogRead(ir_pin);
+  }
+  //takes the summed value and divides it by the number of samples
+  average_sensor_val = sensor_sum/sensor_sample;
 
-  // //print the average sensor value on the serial monitor for troubleshooting
-  // // Serial.print("Sensor Value: ");
-  // // Serial.println(average_sensor_val);
+  //print the average sensor value on the serial monitor for troubleshooting
+  Serial.print("Sensor Value: ");
+  Serial.println(average_sensor_val);
 
-  // //analog value to distance in cm function
-  // //distance = -0.0903*average_sensor_val + 65.306; //old IR sensor function
-  // distance = (pow(2, -9)*pow(average_sensor_val, 4)) - (pow(2, -6)*pow(average_sensor_val, 3)) + (0.0009*pow(average_sensor_val, 2)) - (0.1766*average_sensor_val) + 17.943; //new IR sensor function
+  //analog value to distance in cm function
+  //distance = -0.0903*average_sensor_val + 65.306; //GP2Y0E02A (Phase A) IR sensor function
+  distance = (478.49*(pow(average_sensor_val,-0.866))); //GP2Y0A51SK0F IR sensor function 
+  //print distance value on serial monitor for troubleshooting
+  Serial.print("Distance in cm: ");
+  Serial.println(distance); 
 
-  // //print distance value on serial monitor for troubleshooting
-  // Serial.print("Distance in cm: ");
-  // Serial.println(distance); 
+  //set the sum to 0
+  sensor_sum = 0;
 
-  // //set the sum to 0
-  // sensor_sum = 0;
-
-  // //delay 2 seconds
-  // delay(2000);
+  //delay 2 seconds
+  delay(2000);
 
 
 
