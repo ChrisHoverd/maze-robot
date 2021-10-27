37f5db747deb7e3dfefbf85d723f7dafcb323c14
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-10-26 20:28:44 -0400

    changed encoder/motor objects for new design

--- PID-tests/PID-tests.ino
+++ PID-tests/PID-tests.ino
@@ -8,10 +8,10 @@ The main use of this code is to develop, test, and troubleshoot the wall followi
 
 
 //declare left ir sensor PID constants and error values
-double left_ir_kp = 0.1;
+double left_ir_kp = 5;
 double left_ir_kd = 0;
 double left_ir_ki = 0;
-double left_ir_desired_distance = 60; //keep robot 60mm from wall
+double left_ir_desired_distance = 40; //keep robot 60mm from wall
 volatile double left_ir_distance_error;
 volatile double left_ir_last_distance_error;
 volatile double left_ir_derivative_error;
@@ -24,13 +24,16 @@ int left_ir_pin = A0;
 int front_ir_pin = A1;
 
 //declare IR sampling size and distance
-float sensor_sample = 20;
+float sensor_sample = 50;
 float left_ir_distance;
 
 
 //instantiate the motor objects
-CytronMD leftMotor(PWM_DIR, 5, 4); // Motor 1 EN = Pin 5, DIR = 4
-CytronMD rightMotor(PWM_DIR, 6, 7); // Motor 2 EN = Pin 6, Dir = 7
+// CytronMD leftMotor(PWM_DIR, 5, 4); // EN = Pin 5, DIR = 4
+// CytronMD rightMotor(PWM_DIR, 6, 7); //EN = Pin 6, Dir = 7
+//new pinouts below
+CytronMD leftMotor(PWM_DIR, 6, 7); //EN = Pin 6, Dir = 7
+CytronMD rightMotor(PWM_DIR, 5, 4); // EN = Pin 5, DIR = 4
 
 
 //declare motor PWM values, ie. -255 to 255
@@ -45,7 +48,7 @@ void setup() {
 
   
 //sets the reference voltage for the analog inputs, ie. 3.3V from the arduino for the IR sensors
-analogReference(EXTERNAL);
+// analogReference(EXTERNAL);
   
 //sets the data rate in bits per second (baud)
 Serial.begin(9600);
@@ -86,7 +89,7 @@ left_ir_distance = readIRSensor(left_ir_pin);
 wallFollowPID();
 
 //sets forward power to 70 in PWM
-forward_power = 70;
+forward_power = 80;
 
 //sets the right and left motor speeds according to the turn_rate calculated in the PID function
 right_motor_speed = forward_power - turn_rate;
@@ -94,12 +97,10 @@ left_motor_speed = forward_power + turn_rate;
 
 
 //sets boundaries on how high and low the PWM values sent to the motors can be
-if(right_motor_speed>140)  right_motor_speed = 140;
-//if(right_motor_speed<-100) right_motor_speed = -100;
+if(right_motor_speed>200)  right_motor_speed = 200;
 if(right_motor_speed<60) right_motor_speed = 60;
 
-if(left_motor_speed>140)   left_motor_speed = 140;
-// if(left_motor_speed<-100)  left_motor_speed = -100;
+if(left_motor_speed>200)   left_motor_speed = 200;
 if(left_motor_speed<60)  left_motor_speed = 60;
 
 
@@ -130,16 +131,18 @@ float readIRSensor(int ir_pin)
   //sets a condition that when an analog value <500 (which is the range of values
   // we get when the IR sensor is less than 4cm from a wall which causes incorrect analog values)
   // to set the distance to 20 mm 
-  if(ir_average_val<500)
-  {
-    ir_distance = 20;
-  }
+  // if(ir_average_val<500)
+  // {
+  //   ir_distance = 20;
+  // }
 
   //if the analog value is anything other than <500, do the normal analog to mm calculation
-  else
-  {
-    ir_distance = (-0.0903*ir_average_val + 65.306)*10; //analog to mm function
-  }
+  // else
+  // {
+  //   ir_distance = (-0.0903*ir_average_val + 65.306)*10; //analog to mm function
+  // }
+
+  ir_distance = (478.49*(pow(ir_average_val,-0.866)))*10; //analog to mm function
   
   //returns the ir distance
   return ir_distance;
