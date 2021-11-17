4a560dbb43e4b40ad003f7510895e945f549f3ab
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-11-17 16:35:14 -0500

    updated speed control PID variables for readability

--- speed-PID-tests/speed-PID-tests.ino
+++ speed-PID-tests/speed-PID-tests.ino
@@ -1,28 +1,30 @@
 /*
-The main use of this code is to develop, test, and troubleshoot the wall following PID SPEED algorithim
+The main use of this code is to develop, test, and troubleshoot the wall following PID SPEED algorithim.
+It uses a 
 */
 
 //include motor driver library
 #include "CytronMotorDriver.h" 
 #include "Encoder.h"
 
+//encoder channel pins
 const int left_motor_ch_A = 0;
 const int left_motor_ch_B = 1;
 const int right_motor_ch_A = 2;
 const int right_motor_ch_B = 3;
 
 const float pulses_to_mm = 0.03282036321;
-volatile long last_right_motor_counter = 0;
-volatile long right_motor_counter = 0;
-volatile long right_motor_counter_change;
-volatile long last_left_motor_counter = 0;
-volatile long left_motor_counter = 0;
-volatile long left_motor_counter_change;
+volatile long last_rw_counter = 0;
+volatile long rw_counter = 0;
+volatile long rw_counter_change;
+volatile long last_lw_counter = 0;
+volatile long lw_counter = 0;
+volatile long lw_counter_change;
 
 //declare left ir sensor distance PID constants and error values
-double kp = 5;
-double kd = 0;
-double ki = 0;
+double kp = 2.2;
+double kd = 300;
+double ki = 0.00001;
 double desired_distance = 45;
 volatile double error;
 volatile double last_error;
@@ -34,34 +36,34 @@ volatile double forward_speed;
 //time values for Distance PID
 unsigned long time_now;
 unsigned long time_prev = 0;
-unsigned long time_change;
+double time_change;
 
 //declare left wheel speed PID variables
-int left_wheel_kp = 1;
-int left_wheel_kd = 0;
-int left_wheel_ki = 0;
-double left_wheel_speed_error;
-double left_wheel_speed_derivative_error;
-double last_left_wheel_speed_error;
-double left_wheel_speed_integral_error;
-double left_wheel_pwm;
+int lw_kp = 1;
+int lw_kd = 0;
+int lw_ki = 0;
+double lw_error;
+double lw_derivative_error;
+double last_lw_error;
+double lw_integral_error;
+double lw_pwm;
 
 //declare right wheel speed PID variables
-int right_wheel_kp = 1;
-int right_wheel_kd = 0;
-int right_wheel_ki = 0;
-double right_wheel_speed_error;
-double right_wheel_speed_derivative_error;
-double last_right_wheel_speed_error;
-double right_wheel_speed_integral_error;
-double right_wheel_pwm;
+int rw_kp = 1;
+int rw_kd = 0;
+int rw_ki = 0;
+double rw_error;
+double rw_derivative_error;
+double last_rw_error;
+double rw_integral_error;
+double rw_pwm;
 
 //declare ir values
 int left_ir_pin = A0;
 int right_ir_pin = A2;
 
 //declare IR sampling size and distance
-float sensor_sample = 50;
+int sensor_sample = 50;
 float left_ir_distance;
 float right_ir_distance;
 
@@ -73,41 +75,47 @@ CytronMD rightMotor(PWM_DIR, 5, 4); // EN = Pin 5, DIR = 4
 
 
 //declare motor PWM values, ie. -255 to 255
-volatile int left_motor_speed;
-volatile int right_motor_speed;
+double lw_speed;
+double rw_speed;
 
 double speed;
 
 //time values for left wheel speed PID
-unsigned long left_wheel_time_now;
-unsigned long left_wheel_time_prev = 0;
-unsigned long left_wheel_time_change;
+unsigned long lw_time_now;
+unsigned long lw_time_prev = 0;
+double lw_time_change;
 
 //time values for left wheel speed PID
-unsigned long right_wheel_time_now;
-unsigned long right_wheel_time_prev = 0;
-unsigned long right_wheel_time_change;
+unsigned long rw_time_now;
+unsigned long rw_time_prev = 0;
+double rw_time_change;
 
 
 //time values for printing debugging values
 unsigned long debugging_time_now;
 unsigned long debugging_time_prev = 0;
-unsigned long debugging_time_change;
+double debugging_time_change;
 
 void setup() {
   pinMode (left_motor_ch_A, INPUT_PULLUP);
   pinMode (left_motor_ch_B, INPUT_PULLUP);
   pinMode (right_motor_ch_A, INPUT_PULLUP);
   pinMode (right_motor_ch_B, INPUT_PULLUP);
-  Serial.begin(9600);
+  Serial.begin(115200);
 
 }
 
 void loop() 
 {
-
+delay(40);
 left_ir_distance = readIRSensor(left_ir_pin);
 wallFollowPID();
+//Serial.print("time change: ");
+//Serial.print(time_change);
+//Serial.print(" error ");
+//Serial.print(error);
+//Serial.print(" derivative error: ");
+//Serial.println(derivative_error);
 
 if(turn_rate>255)  turn_rate = 255;
 if(turn_rate<-255) turn_rate = -255;
@@ -116,23 +124,23 @@ pwmToSpeed();
 
 
 forward_speed = 45;
-right_motor_speed = forward_speed - speed;
-left_motor_speed = forward_speed + speed;
-rightWheelSpeedPID(right_motor_speed);
-leftWheelSpeedPID(left_motor_speed);
+rw_speed = forward_speed - speed;
+lw_speed = forward_speed + speed;
+rightWheelSpeedPID(rw_speed);
+leftWheelSpeedPID(lw_speed);
 
 
 //sets boundaries on how high and low the PWM values sent to the motors can be
-if(right_wheel_pwm>255)  right_wheel_pwm = 255;
-if(right_wheel_pwm<0) right_wheel_pwm = 0;
+if(rw_pwm>255)  rw_pwm = 255;
+if(rw_pwm<-255) rw_pwm = -255;
 
-if(left_wheel_pwm>255)   left_wheel_pwm = 255;
-if(left_wheel_pwm<0)  left_wheel_pwm = 0;
+if(lw_pwm>255)   lw_pwm = 255;
+if(lw_pwm<-255)  lw_pwm = -255;
 
 
-// //sends the motor speeds to the right and left motors
-rightMotor.setSpeed(right_wheel_pwm);
-leftMotor.setSpeed(left_wheel_pwm);
+//sends the motor speeds to the right and left motors
+ rightMotor.setSpeed(rw_pwm);
+ leftMotor.setSpeed(lw_pwm);
 }
 
 //this function receives an ir pin value and returns a distance value
@@ -152,6 +160,9 @@ float readIRSensor(int ir_pin)
 
   ir_average_val = ir_sum/sensor_sample;
   ir_distance = (478.49*(pow(ir_average_val,-0.866)))*10; //analog to mm function
+
+  if (ir_distance>150) ir_distance = 150;
+  if (ir_distance<0) ir_distance = 0;
   return ir_distance;
 }
 
@@ -160,8 +171,8 @@ float readIRSensor(int ir_pin)
 // it determines the turn_rate 
 void wallFollowPID()
 {
-  time_now = millis();
-  time_change = time_now - time_prev;
+  time_now = micros();
+  time_change = (float)(time_now - time_prev)/1000;
   error = desired_distance - left_ir_distance;
   derivative_error = (error - last_error)/time_change;
   last_error = error;
@@ -175,11 +186,13 @@ void pwmToSpeed()
 {
   if(turn_rate>0)
   {
-    speed = 5.09 + (2*turn_rate) - (pow(4.56,-3) * pow(turn_rate,2));
+    //speed = 5.09 + (2*turn_rate) - (pow(4.56,-3) * pow(turn_rate,2));old speed function
+    speed = -133 + (6.42 * turn_rate) - (0.0492 * (pow(turn_rate,2))) + ((1.76 * pow(10, -4)) * (pow(turn_rate,3))) - (2.35*(pow(10,-7)) * (pow(turn_rate, 4)));
   }
   if(turn_rate<0)
   {
-    speed = -5.09 + (2*turn_rate) + (pow(4.56,-3) * pow(turn_rate,2));
+    //speed = -5.09 + (2*turn_rate) + (pow(4.56,-3) * pow(turn_rate,2)); old speed function
+    speed = 133 + (6.42 * turn_rate) + (0.0492 * pow(turn_rate,2)) + ((1.76 * pow(10, -4)) * (pow(turn_rate,3))) + (2.35*(pow(10,-7)) * (pow(turn_rate, 4)));
   }
 
 }
@@ -187,33 +200,33 @@ void pwmToSpeed()
 void leftWheelSpeedPID(double desired_speed)
 {
   double desired_left_wheel_speed = desired_speed;
-  left_wheel_time_now = millis();
-  left_wheel_time_change = left_wheel_time_now - left_wheel_time_prev;
-  left_motor_counter = left_Enc.read();
-  left_motor_counter_change = left_motor_counter - last_left_motor_counter;
-  last_left_motor_counter = left_motor_counter;
-  double left_wheel_distance = left_motor_counter_change*pulses_to_mm;
-  double left_wheel_speed = (left_wheel_distance/left_wheel_time_change)*1000;
-  left_wheel_speed_error = desired_left_wheel_speed - left_wheel_speed;
-  left_wheel_speed_derivative_error = (left_wheel_speed_error - last_left_wheel_speed_error)/left_wheel_time_change;
-  last_left_wheel_speed_error = left_wheel_speed_error;
-  left_wheel_speed_integral_error += (left_wheel_speed_error*left_wheel_time_change);
-  left_wheel_pwm = (left_wheel_speed_error*left_wheel_kp) + (left_wheel_speed_derivative_error*left_wheel_kd) + (left_wheel_speed_integral_error*ki);
+  lw_time_now = micros();
+  lw_time_change = (float)(lw_time_now - lw_time_prev)/1000;
+  lw_counter = left_Enc.read();
+  lw_counter_change = lw_counter - last_lw_counter;
+  last_lw_counter = lw_counter;
+  double lw_distance = lw_counter_change*pulses_to_mm;
+  double lw_speed = (lw_distance/lw_time_change)*1000;
+  lw_error = desired_left_wheel_speed - lw_speed;
+  lw_derivative_error = (lw_error - last_lw_error)/lw_time_change;
+  last_lw_error = lw_error;
+  lw_integral_error += (lw_error*lw_time_change);
+  lw_pwm = (lw_error*lw_kp) + (lw_derivative_error*lw_kd) + (lw_integral_error*ki);
 }
 
 void rightWheelSpeedPID(double desired_speed)
 { 
-  double desired_right_wheel_speed = desired_speed;
-  right_wheel_time_now = millis();
-  right_wheel_time_change = right_wheel_time_now - right_wheel_time_prev;
-  right_motor_counter = right_Enc.read();
-  right_motor_counter_change = right_motor_counter - last_right_motor_counter;
-  last_right_motor_counter = right_motor_counter;
-  double right_wheel_distance = right_motor_counter_change*pulses_to_mm;
-  double right_wheel_speed = (right_wheel_distance/right_wheel_time_change)*1000;
-  right_wheel_speed_error = desired_right_wheel_speed - right_wheel_speed;
-  right_wheel_speed_derivative_error = (right_wheel_speed_error - last_right_wheel_speed_error)/right_wheel_time_change;
-  last_right_wheel_speed_error = right_wheel_speed_error;
-  right_wheel_speed_integral_error += (right_wheel_speed_error*right_wheel_time_change);
-  right_wheel_pwm = (right_wheel_speed_error*right_wheel_kp) + (right_wheel_speed_derivative_error*right_wheel_kd) + (right_wheel_speed_integral_error*right_wheel_ki);
+  double desired_rw_speed = desired_speed;
+  rw_time_now = micros();
+  rw_time_change = (float)(rw_time_now - rw_time_prev)/1000;
+  rw_counter = right_Enc.read();
+  rw_counter_change = rw_counter - last_rw_counter;
+  last_rw_counter = rw_counter;
+  double rw_distance = rw_counter_change*pulses_to_mm;
+  double rw_speed = (rw_distance/rw_time_change)*1000;
+  rw_error = desired_rw_speed - rw_speed;
+  rw_derivative_error = (rw_error - last_rw_error)/rw_time_change;
+  last_rw_error = rw_error;
+  rw_integral_error += (rw_error*rw_time_change);
+  rw_pwm = (rw_error*rw_kp) + (rw_derivative_error*rw_kd) + (rw_integral_error*rw_ki);
 }
