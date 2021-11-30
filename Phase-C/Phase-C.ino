e6d062949cffdef36eb5736f01fb05ffe197aac1
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-11-29 23:58:08 -0500

    fixed servo lib issues, updated functions

--- Phase-C/Phase-C.ino
+++ Phase-C/Phase-C.ino
@@ -6,8 +6,8 @@ This code will be used in phase C demonstration
 
 
 #include "CytronMotorDriver.h" 
-#include "VarSpeedServo.h"
 #include "Encoder.h"
+#include "Servo.h"
 
 const int left_motor_ch_A = 0;
 const int left_motor_ch_B = 1;
@@ -15,12 +15,17 @@ const int right_motor_ch_A = 2;
 const int right_motor_ch_B = 3;
 
 const float pulses_to_mm = 0.03282036321;
-//declare encoder counter variables
 volatile long left_motor_counter = 0;
 volatile long right_motor_counter = 0;
 
+Servo obstacleGripper;
+Servo crane;
+
 Encoder left_Enc(left_motor_ch_B, left_motor_ch_A); // swapped channels A & B so that both motors have positive readings when moving forward
 Encoder right_Enc(right_motor_ch_A, right_motor_ch_B);
+
+CytronMD leftMotor(PWM_DIR, 6, 7); //EN = Pin 6, Dir = 7
+CytronMD rightMotor(PWM_DIR, 5, 4); // EN = Pin 5, DIR = 4
 
 //declare left ir sensor PID constants and error values
 // kp = 2.2, kd=1000, ki = 0.00001 stays in middle of lane better but goes around corners unstable
@@ -56,7 +61,6 @@ Encoder right_Enc(right_motor_ch_A, right_motor_ch_B);
 // double ki = 0;
 
 double desired_distance = 45; //use for right angle IR position
-// double desired_distance = 80; //use for 45 degree IR position
 volatile double error;
 volatile double last_error;
 volatile double derivative_error;
@@ -82,11 +86,6 @@ float front_ir_distance;
 float right_ir_distance;
 float back_left_ir_distance;
 
-//instantiate the motor objects
-//new pinouts below
-CytronMD leftMotor(PWM_DIR, 6, 7); //EN = Pin 6, Dir = 7
-CytronMD rightMotor(PWM_DIR, 5, 4); // EN = Pin 5, DIR = 4
-
 //declare motor PWM values, ie. -255 to 255
 volatile int left_motor_speed;
 volatile int right_motor_speed;
@@ -96,31 +95,29 @@ unsigned long debugging_time_now;
 unsigned long debugging_time_prev = 0;
 unsigned long debugging_time_change;
 
-int obstacleGripperSpeed = 50; 
-int craneSpeed = 10;
+int obstacleGripperDelay = 10; 
+int craneDelay = 40;
 
-VarSpeedServo obstacleGripper;
-VarSpeedServo crane;
+int craneBackPos = 0;
+int craneUpPos = 55;
+int craneFrontPos = 145;
 
-int obstacleGripperPos = 0;
-int cranePos = 0;
-
-
-
+int gripperOpenPos = 150;
+int gripperClosePos = 0;
 
 void setup() {
   obstacleGripper.attach(8);
-  crane.attach(10);
+  crane.attach(9);
   pinMode (left_motor_ch_A, INPUT_PULLUP);
   pinMode (left_motor_ch_B, INPUT_PULLUP);
   pinMode (right_motor_ch_A, INPUT_PULLUP);
   pinMode (right_motor_ch_B, INPUT_PULLUP);
   Serial.begin(9600);
-
+  delay(2000);
 // for crane, 0 is the 0 degrees position, 55 is 90 degree position, 145 is 180 degrees position (WITHOUT ADDED WEIGHT OF GRIPPER)
 // for gripper, 0 is fully closed grip and 150 is fully open grip.
-  obstacleGripper.write(0, obstacleGripperSpeed, true); //close gripper
-  crane.write(55, craneSpeed, true); //90 degree crane
+  obstacleGripper.write(gripperClosePos); //close gripper
+  crane.write(craneUpPos); //90 degree crane
 
 }
 
@@ -143,33 +140,49 @@ if (debugging_time_now - debugging_time_prev >= 1000)
 */
 
 //********************************
-  delay(40);
-  left_ir_distance = readIRSensor(left_ir_pin);
-  front_ir_distance = readIRSensor(front_ir_pin);
-  right_ir_distance = readIRSensor(right_ir_pin);
-  wallFollowPID();
+  // delay(40);
+  // left_ir_distance = readIRSensor(left_ir_pin);
+  // front_ir_distance = readIRSensor(front_ir_pin);
+  // right_ir_distance = readIRSensor(right_ir_pin);
+  // wallFollowPID();
 
-  forward_power = 75;
-  // forward_power = 60;
-  right_motor_speed = forward_power - turn_rate;
-  left_motor_speed = forward_power + turn_rate;
+  // forward_power = 75;
+  // // forward_power = 60;
+  // right_motor_speed = forward_power - turn_rate;
+  // left_motor_speed = forward_power + turn_rate;
   
   
-  //sets boundaries on how high and low the PWM values sent to the motors can be
-  if(right_motor_speed>255)  right_motor_speed = 255;
-  if(right_motor_speed<-255) right_motor_speed = -255;
+  // //sets boundaries on how high and low the PWM values sent to the motors can be
+  // if(right_motor_speed>255)  right_motor_speed = 255;
+  // if(right_motor_speed<-255) right_motor_speed = -255;
   
   
-  if(left_motor_speed>255)   left_motor_speed = 255;
-  if(left_motor_speed<-255)  left_motor_speed = -255;
+  // if(left_motor_speed>255)   left_motor_speed = 255;
+  // if(left_motor_speed<-255)  left_motor_speed = -255;
   
   
   
-  // //sends the motor speeds to the right and left motors
-  rightMotor.setSpeed(right_motor_speed);
-  leftMotor.setSpeed(left_motor_speed);
+  // // //sends the motor speeds to the right and left motors
+  // rightMotor.setSpeed(right_motor_speed);
+  // leftMotor.setSpeed(left_motor_speed);
+  obstaclePickup();
+}
+void obstaclePickup()
+{
+  openObstacleGripper();
+  craneMoveFront();
+  closeObstacleGripper();
+  craneMoveUp();
+  rightTurn();
+  craneMoveFront();
+  openObstacleGripper();
+  craneMoveUp();
+  closeObstacleGripper();
+  leftTurn();
 
 }
+
+
 
 void wallFollowPID()
 {
@@ -184,25 +197,7 @@ void wallFollowPID()
   time_prev = time_now;
 }
 
-float readIRSensor(int ir_pin)
-{
 
-  float ir_val = 0;
-  float ir_sum = 0;
-  float ir_average_val = 0;
-  float ir_distance = 0;
-
-  for(int i = 0; i<sensor_sample; ++i)
-  {
-    ir_val = analogRead(ir_pin);
-    ir_sum+= ir_val;
-  }
-
-  ir_average_val = ir_sum/sensor_sample;
-  ir_distance = (478.49*(pow(ir_average_val,-0.866)))*10; //analog to mm function
-  if (ir_distance>150) ir_distance = 150;
-  return ir_distance;
-}
 
 void leftTurn()
 { 
@@ -222,7 +217,6 @@ void leftTurn()
 
   leftMotor.setSpeed(0);
   rightMotor.setSpeed(0);
-  delay(1000);
 }
 
 //this function makes a right turn
@@ -246,7 +240,6 @@ void rightTurn()
 
   leftMotor.setSpeed(0);
   rightMotor.setSpeed(0);
-  delay(1000);
 }
 
 //this function makes a 180 degree turn
@@ -270,5 +263,62 @@ void aboutTurn()
 
   leftMotor.setSpeed(0);
   rightMotor.setSpeed(0);
-  delay(1000);
+}
+float readIRSensor(int ir_pin)
+{
+
+  float ir_val = 0;
+  float ir_sum = 0;
+  float ir_average_val = 0;
+  float ir_distance = 0;
+
+  for(int i = 0; i<sensor_sample; ++i)
+  {
+    ir_val = analogRead(ir_pin);
+    ir_sum+= ir_val;
+  }
+
+  ir_average_val = ir_sum/sensor_sample;
+  ir_distance = (478.49*(pow(ir_average_val,-0.866)))*10; //analog to mm function
+  if (ir_distance>150) ir_distance = 150;
+  return ir_distance;
+}
+
+// int craneBack = 0   ,   int craneUp = 55 , int craneFront = 145
+void craneMoveUp()
+{
+  for(int i = craneFrontPos; i >= craneUpPos; i--)
+  {
+    crane.write(i);
+    delay(craneDelay);
+  }
+}
+
+void craneMoveFront()
+{
+  for(int i = craneUpPos; i <= craneFrontPos; i++)
+  {
+    crane.write(i);
+    delay(craneDelay);
+  }
+}
+
+//int gripperOpenPos = 150     int gripperClosePos = 0
+
+void openObstacleGripper()
+{
+    for(int i = gripperClosePos; i <= gripperOpenPos; i++)
+  {
+    obstacleGripper.write(i);
+    delay(obstacleGripperDelay);
+  }
+}
+
+void closeObstacleGripper()
+{
+    for(int i = gripperOpenPos; i >= gripperClosePos; i--)
+  {
+    obstacleGripper.write(i);
+    delay(obstacleGripperDelay);
+  }
 }
