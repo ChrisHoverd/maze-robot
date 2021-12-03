d927d85a5a9d189a1bb8a8a0b0e9cf6d6e22bb76
Chris Hoverd <christopher.hoverd@ontariotechu.net>
2021-12-03 00:07:03 -0500

    added crane initialization

--- PID-tests/PID-tests.ino
+++ PID-tests/PID-tests.ino
@@ -4,14 +4,55 @@ The main use of this code is to develop, test, and troubleshoot the wall followi
 
 //include motor driver library
 #include "CytronMotorDriver.h" 
-
+#include "Servo.h"
 
 
 //declare left ir sensor PID constants and error values
-double kp = 2.2;
-double kd = 300;
-double ki = 0.00001;
-double desired_distance = 45;
+// kp = 2.2, kd=1000, ki = 0.00001 stays in middle of lane better but goes around corners unstable
+//kp = 2.2, kd=100, ki = 0.00001 turns well but isnt in middle of lane all the time
+//kp = 4, kd = 1, ki = 0 middle ground of turning and staying in middle lane
+
+
+ double kp = 5; 
+ double kd = 500;
+ double ki = 0;
+
+//  double kp = 5; 
+//  double kd = 300;
+//  double ki = 0;
+
+//  double kp = 2.2; 
+//  double kd = 1000;
+//  double ki = 0.00001;
+
+// double kp = 2.2; 
+// double kd = 100;
+// double ki = 0.00001;
+
+// double kp = 2.2; //pretty solid
+// double kd = 300;
+// double ki = 0.00001;
+
+// double kp = 2.2; //pretty solid
+// double kd = 220;
+// double ki = 0.00001;
+
+// double kp = 2.2; //pretty solid
+// double kd = 150;
+// double ki = 0.00001;
+
+//double kp = 3; //pretty solid best so far!!
+//double kd = 150;
+//double ki = 0.00001;
+
+// double kp = 4; 
+// double kd = 1;
+// double ki = 0;
+
+
+
+double desired_distance = 45; //use for right angle IR position
+// double desired_distance = 80; //use for 45 degree IR position
 volatile double error;
 volatile double last_error;
 volatile double derivative_error;
@@ -28,11 +69,15 @@ double time_change;
 int left_ir_pin = A0;
 int front_ir_pin = A1;
 int right_ir_pin = A2;
+int back_left_ir_pin = A3;
+
 //declare IR sampling size and distance
 int sensor_sample = 50;
 float left_ir_distance;
 float front_ir_distance;
 float right_ir_distance;
+float back_left_ir_distance;
+
 
 
 //instantiate the motor objects
@@ -40,6 +85,26 @@ float right_ir_distance;
 CytronMD leftMotor(PWM_DIR, 6, 7); //EN = Pin 6, Dir = 7
 CytronMD rightMotor(PWM_DIR, 5, 4); // EN = Pin 5, DIR = 4
 
+Servo obstacleGripper;
+Servo crane;
+
+int craneBackPos = 0;
+int craneUpPos = 55;
+// int craneFrontPos = 145; // fully front
+int craneFrontPos = 140;
+int cranePartlyFrontPos = 100;
+
+int gripperOpenPos = 150; // fully open
+// int gripperOpenPos = 60;
+int gripperClosePos = 0; // fully closed
+// int gripperClosePos = 50;
+
+int currentCranePos;
+int currentGripperPos; 
+
+int obstacleGripperDelay = 10; 
+int slowObstacleGripperDelay = 40; 
+int craneDelay = 40;
 
 //declare motor PWM values, ie. -255 to 255
 volatile int left_motor_speed;
@@ -51,9 +116,13 @@ unsigned long debugging_time_prev = 0;
 unsigned long debugging_time_change;
 
 void setup() {
-
+  obstacleGripper.attach(8);
+  crane.attach(9);
   Serial.begin(9600);
 
+    initialCraneMoveUp();
+  delay(1000);
+  slowCloseObstacleGripper();
 }
 
 void loop() 
@@ -78,8 +147,9 @@ if (debugging_time_now - debugging_time_prev >= 1000)
 //********************************
 
 
-delay(120);
+delay(40);
 left_ir_distance = readIRSensor(left_ir_pin);
+
 
 //uncomment this section to plot values
 //********************************
@@ -92,7 +162,8 @@ left_ir_distance = readIRSensor(left_ir_pin);
 wallFollowPID();
 
 
-forward_power = 50;
+// forward_power = 75;
+forward_power = 85;
 right_motor_speed = forward_power - turn_rate;
 left_motor_speed = forward_power + turn_rate;
 
@@ -101,8 +172,10 @@ left_motor_speed = forward_power + turn_rate;
 if(right_motor_speed>255)  right_motor_speed = 255;
 if(right_motor_speed<-255) right_motor_speed = -255;
 
+
 if(left_motor_speed>255)   left_motor_speed = 255;
 if(left_motor_speed<-255)  left_motor_speed = -255;
+
 
 
 // //sends the motor speeds to the right and left motors
@@ -145,4 +218,143 @@ void wallFollowPID()
   turn_rate = (kp*error) + (kd*derivative_error) + (ki*integral_error);
   //Serial.println(derivative_error);
   time_prev = time_now;
+}
+
+// int craneBack = 0   ,   int craneUp = 55 , int craneFront = 145
+void craneMoveUp()
+{
+  currentCranePos = crane.read();
+  if (currentCranePos >= craneUpPos)
+  {
+    for(int i = currentCranePos; i >= craneUpPos; i--)
+    {
+      crane.write(i);
+      delay(craneDelay);
+    }
+  }
+  else
+  {
+    for(int i = currentCranePos; i <= craneUpPos; i++)
+    {
+      crane.write(i);
+      delay(craneDelay);
+    }
+  }
+}
+
+void initialCraneMoveUp()
+{
+  for(int i = craneFrontPos; i >= craneUpPos; i--)
+  {
+    crane.write(i);
+    delay(craneDelay);
+  }
+}
+
+void craneMoveFront()
+{
+  currentCranePos = crane.read();
+  if (currentCranePos >= craneFrontPos)
+  {
+    for(int i = currentCranePos; i >= craneFrontPos; i--)
+    {
+      crane.write(i);
+      delay(craneDelay);
+    }
+  }
+  else
+  {
+    for(int i = currentCranePos; i <= craneFrontPos; i++)
+    {
+      crane.write(i);
+      delay(craneDelay);
+    }
+  }
+}
+
+void craneMovePartlyFront()
+{
+  currentCranePos = crane.read();
+  if (currentCranePos >= cranePartlyFrontPos)
+  {
+    for(int i = currentCranePos; i >= cranePartlyFrontPos; i--)
+    {
+      crane.write(i);
+      delay(craneDelay);
+    }
+  }
+  else
+  {
+    for(int i = currentCranePos; i <= cranePartlyFrontPos; i++)
+    {
+      crane.write(i);
+      delay(craneDelay);
+    }
+  }
+}
+
+
+//int gripperOpenPos = 150     int gripperClosePos = 0
+
+void openObstacleGripper()
+{
+  currentGripperPos = obstacleGripper.read();
+  if (currentGripperPos >= gripperOpenPos)
+  {
+    for(int i = currentGripperPos; i >= gripperOpenPos; i--)
+    {
+      obstacleGripper.write(i);
+      delay(obstacleGripperDelay);
+    }
+  }
+  else
+  {
+    for(int i = currentGripperPos; i <= gripperOpenPos; i++)
+    {
+      obstacleGripper.write(i);
+      delay(obstacleGripperDelay);
+    }
+  }
+}
+
+void closeObstacleGripper()
+{ 
+  currentGripperPos = obstacleGripper.read();
+  if (currentGripperPos >= gripperClosePos)
+  {
+    for(int i = currentGripperPos; i >= gripperClosePos; i--)
+    {
+      obstacleGripper.write(i);
+      delay(obstacleGripperDelay);
+    }
+  }
+  else
+  {
+    for(int i = currentGripperPos; i <= gripperClosePos; i++)
+    {
+      obstacleGripper.write(i);
+      delay(obstacleGripperDelay);
+    }
+  }
+}
+
+void slowCloseObstacleGripper()
+{
+  currentGripperPos = obstacleGripper.read();
+  if (currentGripperPos >= gripperClosePos)
+  {
+    for(int i = currentGripperPos; i >= gripperClosePos; i--)
+    {
+      obstacleGripper.write(i);
+      delay(slowObstacleGripperDelay);
+    }
+  }
+  else
+  {
+    for(int i = currentGripperPos; i <= gripperClosePos; i++)
+    {
+      obstacleGripper.write(i);
+      delay(slowObstacleGripperDelay);
+    }
+  }
 }
